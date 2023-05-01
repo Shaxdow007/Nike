30629c49ccde963a855e20c511291159a9400e58
Nour Eddine Houmame <128188124+Shaxdow007@users.noreply.github.com>
2023-05-01 14:07:02 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector(".menu");
+const navBar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  navBar.classList.toggle("show");
+  window.addEventListener("scroll", () => {
+    navBar.classList.remove("show", window.scrollY > 100);
+  });
+});
+// loading page
+const loader = document.querySelector(".loader-page");
+window.addEventListener("load", () => {
+  loader.classList.add("hidden");
+  window.addEventListener("transitionend", () => {
+    document.body.removeChild("loader-page");
+  });
+});
+// fixed nav bar
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("stiky", window.scrollY > 100);
+});
